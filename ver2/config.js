b7fbec4d0d1de94b150a8ba72a20656c23abb1d3
Dmitry <121898072+bpmbpm@users.noreply.github.com>
2026-03-05 13:20:14 +0300

    Update config.js

--- ver2/config.js
+++ ver2/config.js
@@ -17,7 +17,7 @@ window.CONFIG = {
     "borderColor": "darkslategray",
     "edgeColor": "slategray",
     "picDir": "pic",
-    "picDirType": "relative",
+    "picDirType": "relativeGraphvizOnline",
     "picDirGlobal": "",
     "picDirGraphvizOnline": "https://bpmbpm.github.io/family-tree/ver2/pic",
     "language": "ru"
